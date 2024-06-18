2a0c0c7ee19e86fa1b9daa729eb0f258d9e76bd9
Jeferson Zapata Avendaño <jeferosonz1997@gmail.com>
2024-06-18 17:07:51 -0500

    Creación de entidades para conexión a BD festivos

---
+++ src/main/java/apipagonomina/apipagonomina/entidades/Festivo.java
@@ -0,0 +1,112 @@
+package apipagonomina.apipagonomina.entidades;
+
+import java.util.Date;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Table(name = "Festivo")
+public class Festivo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "secuencia_festivo")
+    @GenericGenerator(name = "secuencia_festivo", strategy = "increment")
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "nombre", length = 100, unique = true)
+    private String nombre;
+
+    @Column(name = "dia")
+    private int dia;
+    
+    @Column(name = "mes")
+    private int mes;
+
+    @Column(name = "diaspascua")
+    private int diaspascua;
+    
+    @ManyToOne
+    @JoinColumn(name = "idtipo", referencedColumnName = "id")
+    private Tipo tipo;
+
+    private Date fecha;
+
+    public Festivo() {
+    }
+
+    public Festivo(int id, String nombre, int dia, int mes, int diaspascua, Tipo tipo, Date fecha) {
+        this.id = id;
+        this.nombre = nombre;
+        this.dia = dia;
+        this.mes = mes;
+        this.diaspascua = diaspascua;
+        this.tipo = tipo;
+        this.fecha = fecha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    public int getDiaspascua() {
+        return diaspascua;
+    }
+
+    public void setDiaspascua(int diaspascua) {
+        this.diaspascua = diaspascua;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+}
