5634c04001583605eec3c38e1261e40fc244b45b
Jeferson Zapata Avendaño <jeferosonz1997@gmail.com>
2024-06-18 16:54:03 -0500

    Creación de Scaffold

---
+++ src/main/java/apipagonomina/apipagonomina/ApipagonominaApplication.java
@@ -0,0 +1,13 @@
+package apipagonomina.apipagonomina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApipagonominaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApipagonominaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apipagonomina/apipagonomina/ApipagonominaApplicationTests.java
@@ -0,0 +1,13 @@
+package apipagonomina.apipagonomina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApipagonominaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
